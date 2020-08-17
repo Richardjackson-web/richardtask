05ebb6829062e893271abd551ce575b21d1c51b4
Richardjackson-web <richardjackson556@gmail.com>
2020-08-17 11:10:02 +0100

    added

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let userName = prompt ("What is your name?");
+
+document.getElementById("name").innerHTML = "Welcome" + " " + userName;
+
+
+function openBar() {
+    document.getElementById("end").style.display = "block";
+    document.getElementById("name").style.display = "none";
+}
+
+function closeCross() {
+    document.getElementById("end").style.display = "none";
+    document.getElementById("bars").style.display = "none";
+    document.getElementById("name").style.display = "none";
+    document.getElementById("close").innerHTML = "THANK YOU FOR WAITING" + " " + userName;
+}
---
+++ js/index2.js
@@ -0,0 +1,163 @@
+function click1(){
+    let first = document.getElementById("one").value;
+
+    if (first == "D" || first =="d"){
+        document.getElementById("h1").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h1").style.color = "green"
+        document.getElementById("h1").style.fontFamily = "courier"
+        document.getElementById("h1").style.fontSize = "15px"
+    }
+
+    else{
+        document.getElementById("h1").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h1").style.color = "red"
+        document.getElementById("h1").style.fontFamily = "courier"; 
+        document.getElementById("h1").style.fontSize = "15px"
+    }
+    }
+
+
+function click2(){
+    let first = document.getElementById("two").value;
+    
+    if (first == "C" || first =="c"){
+        document.getElementById("h2").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h2").style.color = "green"
+        document.getElementById("h2").style.fontFamily = "courier"
+        document.getElementById("h2").style.fontSize = "15px"
+    }
+    
+    else{
+        document.getElementById("h2").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h2").style.color = "red"
+        document.getElementById("h2").style.fontFamily = "courier"
+        document.getElementById("h2").style.fontSize = "15px"
+    }
+    }
+
+function click3(){
+    let first = document.getElementById("three").value;
+        
+    if (first == "A" || first =="a"){
+        document.getElementById("h3").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h3").style.color = "green"
+        document.getElementById("h3").style.fontFamily = "courier"
+        document.getElementById("h3").style.fontSize = "15px"
+    }
+        
+    else{
+        document.getElementById("h3").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h3").style.color = "red"
+        document.getElementById("h3").style.fontFamily = "courier"
+        document.getElementById("h3").style.fontSize = "15px"
+    }
+    }
+
+function click4(){
+    let first = document.getElementById("four").value;
+            
+    if (first == "D" || first =="d"){
+        document.getElementById("h4").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h4").style.color = "green"
+        document.getElementById("h4").style.fontFamily = "courier"
+        document.getElementById("h4").style.fontSize = "15px"
+    }
+            
+    else{
+        document.getElementById("h4").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h4").style.color = "red"
+        document.getElementById("h4").style.fontFamily = "courier"
+        document.getElementById("h4").style.fontSize = "15px"
+    }
+    }
+
+function click5(){
+    let first = document.getElementById("five").value;
+                
+    if (first == "B" || first =="b"){
+        document.getElementById("h5").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h5").style.color = "green"
+        document.getElementById("h5").style.fontFamily = "courier"
+        document.getElementById("h5").style.fontSize = "15px"
+    }
+                
+    else{
+        document.getElementById("h5").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h5").style.color = "red"
+        document.getElementById("h5").style.fontFamily = "courier"
+        document.getElementById("h5").style.fontSize = "15px"
+    }
+    }
+
+function click6(){
+    let first = document.getElementById("six").value;
+                    
+    if (first == "C" || first =="c"){
+        document.getElementById("h6").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h6").style.color = "green"
+        document.getElementById("h6").style.fontFamily = "courier"
+        document.getElementById("h6").style.fontSize = "15px"
+    }
+                    
+    else{
+        document.getElementById("h6").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h6").style.color = "red"
+        document.getElementById("h6").style.fontFamily = "courier"
+        document.getElementById("h6").style.fontSize = "15px"
+    }
+    }
+
+function click7(){
+    let first = document.getElementById("seven").value;
+                        
+    if (first == "C" || first =="c"){
+        document.getElementById("h7").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h7").style.color = "green"
+        document.getElementById("h7").style.fontFamily = "courier"
+        document.getElementById("h7").style.fontSize = "15px"
+    }
+                        
+    else{
+        document.getElementById("h7").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h7").style.color = "red"
+        document.getElementById("h7").style.fontFamily = "courier"
+        document.getElementById("h7").style.fontSize = "15px"
+    }
+    }
+
+function click8(){
+    let first = document.getElementById("eight").value;
+                            
+    if (first == "D" || first =="d"){
+        document.getElementById("h8").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h8").style.color = "green"
+        document.getElementById("h8").style.fontFamily = "courier"
+        document.getElementById("h8").style.fontSize = "15px"
+                            
+    }
+                            
+    else{
+        document.getElementById("h8").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h8").style.color = "red"
+        document.getElementById("h8").style.fontFamily = "courier"
+        document.getElementById("h8").style.fontSize = "15px"
+    }
+    }
+
+function click9(){
+        let first = document.getElementById("nine").value;
+                                
+    if (first == "C" || first =="c"){
+        document.getElementById("h9").innerHTML = "&#10004 Correct! Next question."
+        document.getElementById("h9").style.color = "green"
+        document.getElementById("h9").style.fontFamily = "courier"
+        document.getElementById("h9").style.fontSize = "15px"
+    }
+                                
+    else{
+        document.getElementById("h9").innerHTML = "&#10060 Incorrect! Try again."
+        document.getElementById("h9").style.color = "red"
+        document.getElementById("h9").style.fontFamily = "courier"
+        document.getElementById("h9").style.fontSize = "15px"
+    }
+    }
